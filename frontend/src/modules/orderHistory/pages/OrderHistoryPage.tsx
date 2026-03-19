901174edbe0985df87b0b776de09c22d9af8601d
prowider5565 <prowider.dev@gmail.com>
2026-03-19 14:01:25 +0500

    asdasd

--- frontend/src/modules/orderHistory/pages/OrderHistoryPage.tsx
+++ frontend/src/modules/orderHistory/pages/OrderHistoryPage.tsx
@@ -75,7 +75,7 @@ export default function OrderHistoryPage({
       <Box
         sx={{
           p: 2,
-          pb: 12,
+          pb: 8,
           height: { xs: 'calc(100vh - 56px)', sm: 'calc(100vh - 64px)' },
           display: 'flex',
           flexDirection: 'column',
